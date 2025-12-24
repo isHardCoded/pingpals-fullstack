b2a385d00823aece77455f3249917b03c18eebd0
isHardCoded <ishardcoded@gmail.com>
2025-12-24 11:51:34 +0300

    Фикс constructor param

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -3,11 +3,7 @@ import type { User as UserModel } from '../models/User/User.ts';
 import { CreateUserDto, GetUserDto } from '../dto/user/index.js';
 
 export class UserService {
-  private userModel: ModelStatic<UserModel>;
-
-  constructor(userModel: ModelStatic<UserModel>) {
-    this.userModel = userModel;
-  }
+  constructor(private userModel: ModelStatic<UserModel>) {}
 
   getUser = async (obj: GetUserDto) => {
     return await this.userModel.findOne({
