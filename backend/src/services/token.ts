b9d46442c33641d747a34bf8b916c568666b5257
isHardCoded <ishardcoded@gmail.com>
2025-12-27 22:39:33 +0300

    Создали токен сервис

---
+++ backend/src/services/token.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import { Token } from '../models/Token/Token.js';
+
+export class TokenService {
+  generateTokens(payload: { id: number }) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET!, {
+      expiresIn: '15m',
+    });
+
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET!, {
+      expiresIn: '30d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+}
