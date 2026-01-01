d555f0eb23fe02c666065945521c55095a400eff
isHardCoded <ishardcoded@gmail.com>
2026-01-02 00:11:47 +0300

    Добавили шрифт и переменные

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { Button } from './atoms';
 
 export function App() {
   return (
-    <div style={{ display: 'flex', gap: 50, padding: 30 }}>
-      <Button>Hello!</Button>
-      <Button appearance="secondary">Hello!</Button>
-      <Button disabled>Hello!</Button>
-      <Button disabled appearance="secondary">
-        Hello!
-      </Button>
+    <div style={{ padding: 30, fontFamily: 'Poppins, sans-serif' }}>
+      <div style={{ marginTop: 50, display: 'flex', gap: 50 }}>
+        <Button>Hello!</Button>
+        <Button appearance="secondary">Hello!</Button>
+        <Button disabled>Hello!</Button>
+        <Button disabled appearance="secondary">
+          Hello!
+        </Button>
+      </div>
     </div>
   );
 }
