9549c432d7959ac3c51fa1c365a41f0003350f88
isHardCoded <ishardcoded@gmail.com>
2026-01-03 15:04:17 +0300

    Сверстали модалку

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import { Modal } from "./components/ui/modal";
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <></>
+  const [open, setOpen] = useState(true)
+
+  return <>
+    <Modal isOpen={open} title="Заголовок" description="Это контет модалки" onClose={() => setOpen(false)}
+  secondaryAction={{
+    label: 'Отмена',
+    onClick: () => setOpen(false),
+  }}
+  primaryAction={{
+    label: 'Удалить',
+    onClick: () => alert('Удаление'),
+  }} />
+  </>
 }
