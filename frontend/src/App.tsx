d8062bb3b66f62b08420ccfcc18cff7a4f2cf36c
isHardCoded <ishardcoded@gmail.com>
2026-01-07 01:30:27 +0300

    Добавили токены shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
 
 export function App() {
   return (
     <>
       <Input placeholder="Text" />
+      <Button>Click</Button>
     </>
   );
 }
