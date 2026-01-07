789829755633a95dcb442fa6b6afe5905fa1c0d6
isHardCoded <ishardcoded@gmail.com>
2026-01-07 18:42:32 +0300

    Создали компонент Tab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,22 @@
+import { Tab } from './components/ui/tab';
+import { useState } from 'react';
+
 export function App() {
-  return <></>
+  const [activeTab, setActiveTab] = useState<number>(1);
+
+  return (
+    <div style={{ padding: 40, display: 'flex', gap: 12 }}>
+      <Tab id={1} active={activeTab === 1} onClick={setActiveTab}>
+        Overview
+      </Tab>
+
+      <Tab id={2} active={activeTab === 2} onClick={setActiveTab}>
+        Details
+      </Tab>
+
+      <Tab id={3} active={activeTab === 3} onClick={setActiveTab}>
+        Settings
+      </Tab>
+    </div>
+  );
 }
